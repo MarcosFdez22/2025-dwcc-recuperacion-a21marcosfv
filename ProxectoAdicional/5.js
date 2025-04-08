00940bee9da91b8ffb2e5743c57fd83cccf8fbdc
Marcos Fdez <a21marcosfv@iessanclemente.net>
2025-04-08 10:29:58 +0200

    a

--- ProxectoAdicional/5.js
+++ ProxectoAdicional/5.js
@@ -1,10 +1,13 @@
+function dividirArray(array, numero) {
+    let resultado = [];
+    for (let i = 0; i < array.length; i += numero) {
+        let trozo = array.slice(i, i + numero);
+        resultado.push(trozo);
+    }
 
+    return resultado;
+}
 
-let array = [1, 2, 3, 4, 5, 6, 7]
-let numero = 5
-
-let tamaño = array.length
-
-let divisiones = Math.ceil(tamaño / numero)
-
-
+console.log(dividirArray([1, 2, 3, 4], 2)); 
+console.log(dividirArray([1, 2, 3, 4], 3)); 
+console.log(dividirArray([1, 2, 3, 4, 5, 6, 7], 5));
